8a63c60ee879aaa5238e6958fd7dc7cb7c5079aa
tomoki shiozawa <shiotomo27@collesta.com>
2023-02-21 10:32:56 +0900

    ウェブサイトのmetaデータの設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,20 @@ export default defineNuxtConfig({
   build: {
     transpile: ["vuetify"],
   },
+  app: {
+    head: {
+      charset: 'utf-16',
+      viewport: 'width=device-width',
+      title: 'ぐうたらまにあ',
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: 'favicon//favicon.ico' }
+      ],
+      htmlAttrs: {
+        lang: 'ja',
+        prefix: 'og: https://ogp.me/ns#'
+      },
+    }
+  },
   vite: {
     define: {
       "process.env.DEBUG": false,
