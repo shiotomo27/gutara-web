d074e95f73e9f2ac6f3d8ae53ab23e4c1a499886
tomoki shiozawa <shiotomo27@collesta.com>
2023-02-25 23:21:05 +0900

    contract を本番に差し替え

--- pages/mint.vue
+++ pages/mint.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="pa-6 pb-12 d-flex flex-column justify-center align-center">
     <iframe
-      src="https://gateway.ipfscdn.io/ipfs/QmRHAgPic1HeakAw9EU7WRjt4NPE19pWb8hCorRNhw4Zdy/erc721.html?contract=0xe40881fEbA9E43e48A007B3F55C709861CF74168&chainId=80001"
+      src="https://gateway.ipfscdn.io/ipfs/QmbAgC8YwY36n8H2kuvSWsRisxDZ15QZw3xGZyk9aDvcv7/erc721.html?contract=0x9F44D66be3FBfde4Eb7c7bbb67A15ea09Dbd384c&chain=%7B%22name%22%3A%22Ethereum+Mainnet%22%2C%22chain%22%3A%22ETH%22%2C%22rpc%22%3A%5B%22https%3A%2F%2Fethereum.rpc.thirdweb.com%2F5a9bc94b87f7cbbbfbbc234bf1e07f0adf5f3cf3012c9f26f9fc9820d64df93a%22%5D%2C%22nativeCurrency%22%3A%7B%22name%22%3A%22Ether%22%2C%22symbol%22%3A%22ETH%22%2C%22decimals%22%3A18%7D%2C%22shortName%22%3A%22eth%22%2C%22chainId%22%3A1%2C%22testnet%22%3Afalse%2C%22slug%22%3A%22ethereum%22%7D"
       width="600px"
       height="600px"
       style="max-width: 100%"
